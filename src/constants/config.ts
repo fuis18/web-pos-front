3099d1c00cae8759738c719613fbd5ef48a45fcb
fuis18 <warfuis18larcz018@gmail.com>
2026-03-26 19:59:34 -0500

    update token

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
 	API_BASE: import.meta.env.VITE_API_BASE ?? "/api",
 	LIMIT: 10,
-	TOKEN: "123456",
+	TOKEN: "130285",
 	GITHUB_PROFILE_URL: "https://github.com/fuis18/",
 	GITHUB_REPO_URL: "https://github.com/fuis18/app-pos",
 };
