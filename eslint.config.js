97216bc4609a405c2264ff2086ae5b82a53d1269
fuis18 <warfuis18larcz018@gmail.com>
2026-03-26 21:22:30 -0500

    using eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-//  tseslint.configs.recommendedTypeChecked,
 //  tseslint.configs.strictTypeChecked,
 //  tseslint.configs.stylisticTypeChecked,
-
-// languageOptions: {
-//       // parserOptions: {
-//       //   project: ['./tsconfig.node.json', './tsconfig.app.json'],
-//       //   tsconfigRootDir: import.meta.dirname,
-//       // },
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -27,8 +17,13 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      tseslint.configs.recommendedTypeChecked,
     ],
     languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
